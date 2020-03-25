372d0ca2cd11258dacfda39e77b2dc1696dc4bfd
Torok Tibor DANIEL <daniel.torok54@yahoo.com>
2020-03-25 12:54:06 +0200

    Last commit

--- LaboratorWCF/ClientPostComment/Form1.Designer.cs
+++ LaboratorWCF/ClientPostComment/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgp = new System.Windows.Forms.DataGridView();
+            this.dgc = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgp
+            // 
+            this.dgp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgp.Location = new System.Drawing.Point(12, 12);
+            this.dgp.Name = "dgp";
+            this.dgp.RowTemplate.Height = 24;
+            this.dgp.Size = new System.Drawing.Size(382, 426);
+            this.dgp.TabIndex = 0;
+            // 
+            // dgc
+            // 
+            this.dgc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgc.Location = new System.Drawing.Point(409, 12);
+            this.dgc.Name = "dgc";
+            this.dgc.RowTemplate.Height = 24;
+            this.dgc.Size = new System.Drawing.Size(379, 426);
+            this.dgc.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgc);
+            this.Controls.Add(this.dgp);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgc)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgp;
+        private System.Windows.Forms.DataGridView dgc;
     }
 }
 
--- LaboratorWCF/ClientPostComment/Form1.cs
+++ LaboratorWCF/ClientPostComment/Form1.cs
@@ -12,9 +12,6 @@ namespace ClientPostComment
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        
     }
 }
